b274455c00573ff1e0452e1b5b49070fbe94dd8a
John Hollowell <jhollowe@johnhollowell.com>
2025-02-22 04:09:10 +0000

    Add ifan04

---
+++ components/ifan04/ifan04.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace ifan04 {
+
+class IFan04 : public Component, public uart::UARTDevice {
+public:
+  void dump_config() override;
+  void loop() override;
+
+  Trigger<int> *get_fan_trigger() const { return fan_trigger_; }
+  Trigger<> *get_light_trigger() const { return light_trigger_; }
+  Trigger<> *get_buzzer_trigger() const { return buzzer_trigger_; }
+
+protected:
+  void handle_char_(uint8_t c);
+  void handle_command_(uint8_t type, uint8_t param);
+
+  Trigger<int> *fan_trigger_ = new Trigger<int>();
+  Trigger<> *light_trigger_ = new Trigger<>();
+  Trigger<> *buzzer_trigger_ = new Trigger<>();
+};
+
+} // namespace ifan04
+} // namespace esphome
