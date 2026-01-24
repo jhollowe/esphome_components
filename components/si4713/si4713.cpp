a26a59a79e4a9747c384bc1cd1e8c6e8f9fad383
John Hollowell <jhollowe@johnhollowell.com>
2026-01-24 21:25:28 +0000

    Add alternating RSD PS messages

--- components/si4713/si4713.cpp
+++ components/si4713/si4713.cpp
@@ -65,14 +65,21 @@ void Si4713Hub::setup() {
   this->enabled_ = true;
   this->set_power(100);
   this->print_tune_status(this->get_tune_status());
-  // this->set_property(SI4713_PROP_TX_COMPONENT_ENABLE, 0x7);  // Enable pilot, L-R, and RDS
 
   this->setup_rds(0x27CB, 9);  // program ID KJAH, PTY=9 (top 40)
   uint8_t ps1_1[] = {0x0, 'J', 'H', 'O', 'L'};
   this->write_register(SI4710_CMD_TX_RDS_PS, ps1_1, sizeof(ps1_1));
+  this->wait_for_cts_();
   uint8_t ps1_2[] = {0x1, 'L', 'O', 'W', 'E'};
   this->write_register(SI4710_CMD_TX_RDS_PS, ps1_2, sizeof(ps1_2));
-  this->set_property(SI4713_PROP_TX_RDS_MESSAGE_COUNT, 1);  // 1 PS message
+  this->wait_for_cts_();
+  uint8_t ps2_1[] = {0x2, ' ', 'R', 'a', 'd'};
+  this->write_register(SI4710_CMD_TX_RDS_PS, ps2_1, sizeof(ps2_1));
+  this->wait_for_cts_();
+  uint8_t ps2_2[] = {0x3, 'i', 'o', 'F', 'M'};
+  this->write_register(SI4710_CMD_TX_RDS_PS, ps2_2, sizeof(ps2_2));
+  this->wait_for_cts_();
+  this->set_property(SI4713_PROP_TX_RDS_MESSAGE_COUNT, 2);  // num of PS messages
 }
 
 void Si4713Hub::update() {
@@ -102,7 +109,7 @@ void Si4713Hub::update() {
   }
 
   // DEBUG
-  this->print_asq_status(asq_status_curr_);
+  // this->print_asq_status(asq_status_curr_);
   this->print_tune_status(tune_status_curr_);
 
   // iterate through next properties and notify listeners if the value is different from current
@@ -338,9 +345,9 @@ void Si4713Hub::setup_rds(uint16_t programID, uint8_t pty) {
   this->set_property(SI4713_PROP_TX_RDS_PI, programID);       // program identifier
   this->set_property(SI4713_PROP_TX_RDS_PS_MIX, 0x03);        // 50% mix (default)
   this->set_property(SI4713_PROP_TX_RDS_PS_REPEAT_COUNT, 3);  // 3 repeats (default)
-  this->set_property(SI4713_PROP_TX_RDS_MESSAGE_COUNT, 1);    // 1 message (default)
-  this->set_property(SI4713_PROP_TX_RDS_PS_AF, 0xE0E0);       // no AF (default)
-  this->set_property(SI4713_PROP_TX_RDS_FIFO_SIZE, 0);        // no FIFO (default)
+  // this->set_property(SI4713_PROP_TX_RDS_MESSAGE_COUNT, 1);    // 1 message (default)
+  this->set_property(SI4713_PROP_TX_RDS_PS_AF, 0xE0E0);  // no AF (default)
+  this->set_property(SI4713_PROP_TX_RDS_FIFO_SIZE, 0);   // no FIFO (default)
   // this->set_property(SI4713_PROP_TX_COMPONENT_ENABLE, 0x0007);  // enable RDS, stereo, tone
   // 0 PTY is static
   // 0 not compressed
